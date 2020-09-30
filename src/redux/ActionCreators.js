7a6144a4c6a131f352ebc91f9aaa45b6cd668291
sabrinatouch <sabrinajindatouch@gmail.com>
2020-09-30 15:34:57 -0600

    Redux Thunk

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,6 +1,7 @@
 // You send these Actions to the Store
 
 import * as ActionTypes from './ActionTypes';
+import { DISHES } from '../shared/dishes';
 
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -10,4 +11,28 @@ export const addComment = (dishId, rating, author, comment) => ({
         author: author,
         comment: comment
     } 
+});
+
+// This is returning a function
+export const fetchDishes = () => (dispatch) => {
+    dispatch(dishesLoading(true));
+
+    setTimeout(() => {
+        dispatch(addDishes(DISHES));
+    }, 2000);
+}
+
+// These are returning an action
+export const dishesLoading = () => ({
+    type: ActionTypes.DISHES_LOADING
+});
+
+export const dishesFailed = (errmess) => ({
+    type: ActionTypes.DISHES_FAILED,
+    payload: errmess
+});
+
+export const addDishes = (dishes) => ({
+    type: ActionTypes.ADD_DISHES,
+    payload: dishes
 });
--- src/redux/ActionTypes.js
+++ src/redux/ActionTypes.js
@@ -4,4 +4,7 @@
 // And then use that to do the matching in the switch statement
 
 export const ADD_COMMENT = 'ADD_COMMENT';
+export const DISHES_LOADING = 'DISHES_LOADING';
+export const DISHES_FAILED = 'DISHES_FAILED';
+export const ADD_DISHES = 'ADD_DISHES';
 
