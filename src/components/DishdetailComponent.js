bfeba0db04f260ead0267f230f31ba170f688a41
sabrinatouch <sabrinajindatouch@gmail.com>
2020-10-01 14:56:19 -0600

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -22,7 +22,7 @@ function RenderDish({dish}) {
     );
 }
 
-function RenderComments({comments, addComment, dishId}) {
+function RenderComments({comments, postComment, dishId}) {
     return (
         <div className="col-12 col-md-5 m-1 text-left">
             <h4>Comments</h4>
@@ -34,7 +34,7 @@ function RenderComments({comments, addComment, dishId}) {
                     </ul>
                 </div>
             ))}
-            <CommentForm dishId={dishId} addComment={addComment} />
+            <CommentForm dishId={dishId} postComment={postComment} />
         </div>
     )
 }
@@ -59,7 +59,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -169,7 +169,7 @@ const DishDetail = (props) => {
                 <div className="row">
                     <RenderDish dish={props.dish} />
                     <RenderComments comments={props.comments} 
-                        addComment={props.addComment} 
+                        postComment={props.postComment} 
                         dishId={props.dish.id}
                     />
                 </div>
